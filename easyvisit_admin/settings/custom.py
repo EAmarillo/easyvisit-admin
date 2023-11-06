0a30f7334c35d71d99571a09aeaf3140972b0e6c
eamarillo <eamarillob@gmail.com>
2023-11-06 15:19:23 -0600

    settings refactored to allow .env files

---
+++ easyvisit_admin/settings/custom.py
@@ -0,0 +1,31 @@
+from .common import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG')
+
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
+# Internationalization
+
+TIME_ZONE = 'America/Mexico_City'
